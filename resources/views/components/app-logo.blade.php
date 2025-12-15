949ecf5b9ded08ade08b9f665b6a28211c92dc94
Andhika <Dhika.putratama@gmail.com>
2025-12-16 01:10:52 +0700

    chore: center text logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,6 @@
 {{-- <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div> --}}
-<div class="ms-1 grid flex-1 text-start text-2xl">
+<div class="ms-1 grid flex-1 text-center text-2xl">
     <span class="mb-0.5 truncate leading-tight font-semibold">RetailPOS</span>
 </div>
